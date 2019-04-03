585d0d529ea3f64a60329dfc7d65cbae87100db4
rolaxxxx <rolandas.ulevicius@gmail.com>
2019-04-03 13:06:41 +0300

    pakavimas-matematika-update

--- src/core/Math.cxx
+++ src/core/Math.cxx
@@ -9,7 +9,69 @@ PointsArrayType Math::getSpheresTouchingThreeOtherSpheres(PointType p1,PointType
 {
     PointsArrayType data;
 
+    INT spind;
 
+    REAL ds1, ds2, ds3;
+    Point S1, S2, S3;
+    Point rez_sphere_coords_positive, rez_sphere_coords_negative;
+    REAL saved_R;
+    spind=r;
+    //rez_sphere_coords[3]=particle.Distribution[spind];
+    rez_sphere_coords_positive.R=1;
+    rez_sphere_coords_negative.R=1;
+    saved_R=rez_sphere_coords_positive.R;
+
+    S1=p1;
+    S2=p2;
+    S3=p3;
+
+    ds1=S1.R+rez_sphere_coords_positive.R;
+    ds2=S2.R+rez_sphere_coords_positive.R;
+    ds3=S3.R+rez_sphere_coords_positive.R;
+
+    //cout << S1 << " " << S2 << " " << S3 << endl;
+    Point vector_u, vector_v, vector_w;
+    Point temporS1, temporS3;
+
+    temporS1=S1-S2;
+    temporS3=S1-S3;
+
+    vector_u=temporS1/(sqrt(pow(temporS1.x,2)+pow(temporS1.y,2)+pow(temporS1.z,2)));
+    vector_v=temporS3/(sqrt(pow(temporS3.x,2)+pow(temporS3.y,2)+pow(temporS3.z,2)));
+
+    //cout << vector_w << endl;
+    vector_w=S1*(-2);
+    //cout << vector_w << endl;
+
+    REAL calc_a, calc_b, calc_c;
+    calc_a=(pow(ds2,2)-pow(ds1,2)+pow(S1.x,2)-pow(S2.x,2)+pow(S1.y,2)-pow(S2.y,2)+pow(S1.z,2)-pow(S2.z,2))/
+           (2*sqrt(pow(temporS1.x,2)+pow(temporS1.y,2)+pow(temporS1.z,2)));
+    calc_b=(pow(ds3,2)-pow(ds1,2)+pow(S1.x,2)-pow(S3.x,2)+pow(S1.y,2)-pow(S3.y,2)+pow(S1.z,2)-pow(S3.z,2))/
+           (2*sqrt(pow(temporS3.x,2)+pow(temporS3.y,2)+pow(temporS3.z,2)));
+    calc_c=pow(ds1,2)-pow(S1.x,2)-pow(S1.y,2)-pow(S1.z,2);
+
+    Point vector_t;
+    Point xProd=cross_prod(vector_u, vector_v);
+    vector_t=xProd/(sqrt(pow(xProd.x,2)+pow(xProd.y,2)+pow(xProd.z,2)));
+    //cout << vector_t<< endl;
+
+    REAL rez_calc_a, rez_calc_b, rez_calc_c, rez_calc_d_positive, rez_calc_d_negative;
+    rez_calc_a=(calc_a-calc_b*dot_prod(vector_u, vector_v))/(1-pow(dot_prod(vector_u, vector_v),2));
+    rez_calc_b=(calc_b-calc_a*dot_prod(vector_u, vector_v))/(1-pow(dot_prod(vector_u, vector_v),2));
+
+    rez_calc_c=pow(rez_calc_a,2)+pow(rez_calc_b,2)+2*rez_calc_a*rez_calc_b*dot_prod(vector_u, vector_v)
+           +rez_calc_a*dot_prod(vector_u, vector_w)+rez_calc_b*dot_prod(vector_v, vector_w)-calc_c;
+
+    rez_calc_d_positive=(0.5)*(-dot_prod(vector_w, vector_t)+sqrt(pow(dot_prod(vector_w, vector_t),2)-4*rez_calc_c)); // ar reikia minusines reiksmes
+    rez_calc_d_negative=(0.5)*(-dot_prod(vector_w, vector_t)-sqrt(pow(dot_prod(vector_w, vector_t),2)-4*rez_calc_c));
+
+    rez_sphere_coords_positive=rez_calc_a*vector_u+rez_calc_b*vector_v+rez_calc_d_positive*vector_t;
+    rez_sphere_coords_negative=rez_calc_a*vector_u+rez_calc_b*vector_v+rez_calc_d_negative*vector_t;
+
+    rez_sphere_coords_positive.R=1;
+    rez_sphere_coords_negative.R=1;
+    data.push_back(rez_sphere_coords_positive);
+    data.push_back(rez_sphere_coords_negative);
     /// Tavo kodas
     return data;
 }
--- src/core/Point.cxx
+++ src/core/Point.cxx
@@ -52,15 +52,20 @@ Point operator - (Point A,  Point B )
             A.R-=B.R;
              return A;
 }
-REAL dot_prod(Point &vec_a, Point &vec_b){  // dot produkto skaiciavimas
+REAL dot_prod(Point vec_a, Point vec_b){  // dot produkto skaiciavimas
     REAL dot_prod_rez;
     dot_prod_rez=vec_a.x*vec_b.x + vec_a.y*vec_b.y + vec_a.z*vec_b.z;
     return dot_prod_rez;
 }
-Point cross_prod(Point &vec_a, Point &vec_b){ //  cross produkto skaiciavimas
+Point cross_prod(Point vec_a, Point vec_b){ //  cross produkto skaiciavimas
     Point prod_rez;
     prod_rez.x=vec_a.y*vec_b.z-vec_a.z*vec_b.y;
     prod_rez.y=vec_a.z*vec_b.x-vec_a.x*vec_b.z;
     prod_rez.z=vec_a.x*vec_b.y-vec_a.y*vec_b.x;
     return prod_rez;
+}
+
+REAL vector_len(Point vec){
+    REAL len;
+    len=sqrt(pow(vec.x,2)+pow(vec.y,2)+pow(vec.z,2));
 }
