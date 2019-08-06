0b2f614c903429b6e4a14fbee4eb3d27d159d514
rolaxxxx <rolandas.ulevicius@gmail.com>
2019-08-06 12:31:05 +0300

    packing-update

--- src/Boundaries/Mesh.cpp
+++ src/Boundaries/Mesh.cpp
@@ -3,7 +3,21 @@
 Mesh::Mesh(){
     
 }
-
+bool Mesh::check(Point p, Point a, Point b, Point c){
+    a -= p; b -= p; c -= p;
+    float ab = dot_prod(a, b);
+    float ac = dot_prod(a, c);
+    float bc = dot_prod(b, c);
+    float cc = dot_prod(c, c);
+    // Make sure plane normals for pab and pbc point in the same direction
+    if (((bc * ac) - (cc * ab)) < 0.0f)
+        return 0;
+    // Make sure plane normals for pab and pca point in the same direction
+    float bb = dot_prod(b, b);
+    if (((ab * bc) - (ac * bb)) < 0.0f) return 0;
+    // Otherwise P must be in (or on) the triangle
+    return 1;
+}
 
 
 
--- src/Boundaries/cylindershape.cpp
+++ src/Boundaries/cylindershape.cpp
@@ -13,62 +13,6 @@ bool CylinderShape::check(Point newSphere){
             return true;
         else return false;
 }
-
-/*bool CylinderShape::check(Point newSphere )
-{
-    float dx, dy, dz;	// vector d  from line segment point 1 to point 2
-    float pdx, pdy, pdz;	// vector pd from point 1 to test point
-    float dot, dsq;
-
-    dx = max.x - min.x;	// translate so min is origin.  Make vector from
-    dy = max.y - min.y;     // min to max.  Need for this is easily eliminated
-    dz = max.z - min.z;
-    //newSphere.PrintStructure();
-    pdx = newSphere.x - min.x;		// vector from min to test point.
-    pdy = newSphere.y - min.y;
-    pdz = newSphere.z - min.z;
-
-    // Dot the d and pd vectors to see if point lies behind the
-    // cylinder cap at min.x, min.y, min.z
-
-    dot = pdx * dx + pdy * dy + pdz * dz;
-
-    // If dot is less than zero the point is behind the min cap.
-    // If greater than the cylinder axis line segment length squared
-    // then the point is outside the other end cap at max.
-
-    if( dot < 0.0f || dot > height )
-    {
-        return( -1.0f );
-    }
-    else
-    {
-        // Point lies within the parallel caps, so find
-        // distance squared from point to line, using the fact that sin^2 + cos^2 = 1
-        // the dot = cos() * |d||pd|, and cross*cross = sin^2 * |d|^2 * |pd|^2
-        // Carefull: '*' means mult for scalars and dotproduct for vectors
-        // In short, where dist is pt distance to cyl axis:
-        // dist = sin( pd to d ) * |pd|
-        // distsq = dsq = (1 - cos^2( pd to d)) * |pd|^2
-        // dsq = ( 1 - (pd * d)^2 / (|pd|^2 * |d|^2) ) * |pd|^2
-        // dsq = pd * pd - dot * dot / LENGTHSQ
-        //  where LENGTHSQ is d*d or |d|^2 that is passed into this function
-
-        // distance squared to the cylinder axis:
-
-        dsq = (pdx*pdx + pdy*pdy + pdz*pdz) - dot*dot/height;
-
-        if( dsq > radius )
-        {
-            false;
-        }
-        else
-        {
-            true;	// return distance squared to axis
-        }
-    }
-}
-*/
 void CylinderShape::initialization(json duomenys)
 {
 
