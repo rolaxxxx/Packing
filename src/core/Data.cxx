a2daf83dbde8031d83bef8b5ef548f0a2d05ad50
Rolandas Ulevicius <rolandas.ulevicius@stud.vgtu.lt>
2019-11-06 21:25:26 +0200

    json checker update- reikia papildyti dar - json config file update

--- src/core/Data.cxx
+++ src/core/Data.cxx
@@ -36,7 +36,47 @@ bool Data::Json_cheker(json duomenys, std::string file){
 						else std::cerr<<"Klaida   DISTRIBUTION/RMIN REIKSME NERASTA  \n";
 				}
 					else std::cerr<<"Klaida   DISTRIBUTION/RMAX REIKSME NERASTA  \n";
-			}
+		}
+		else if(duomenys["BOUNDARIES"]["TYPE"]=="Cube")
+			if(file.find("RMAX") != std::string::npos){
+				if(file.find("RMIN") != std::string::npos){
+					if(file.find("RADIUS") != std::string::npos){
+						if(file.find("AMOUNT_PARTICLES") != std::string::npos){
+							if(file.find("TEMP_REZULTS_SWITCH") != std::string::npos){
+								
+									
+								}
+									else std::cerr<<"Klaida   WRITER/TEMP_REZULTS_SWITCH REIKSME NERASTA  \n";
+							}
+								else std::cerr<<"Klaida   WRITER/AMOUNT_PARTICLES REIKSME NERASTA  \n";
+						
+						}	
+							else std::cerr<<"Klaida   DISTRIBUTION/RADIUS REIKSME NERASTA  \n";
+					}
+						else std::cerr<<"Klaida   DISTRIBUTION/RMIN REIKSME NERASTA  \n";
+				}
+					else std::cerr<<"Klaida   DISTRIBUTION/RMAX REIKSME NERASTA  \n";
+					
+		else if(duomenys["BOUNDARIES"]["TYPE"]=="Cylinder")
+			if(file.find("RMAX") != std::string::npos){
+				if(file.find("RMIN") != std::string::npos){
+					if(file.find("RADIUS") != std::string::npos){
+						if(file.find("AMOUNT_PARTICLES") != std::string::npos){
+							if(file.find("TEMP_REZULTS_SWITCH") != std::string::npos){
+								
+									
+								}
+									else std::cerr<<"Klaida   WRITER/TEMP_REZULTS_SWITCH REIKSME NERASTA  \n";
+							}
+								else std::cerr<<"Klaida   WRITER/AMOUNT_PARTICLES REIKSME NERASTA  \n";
+						
+						}	
+							else std::cerr<<"Klaida   DISTRIBUTION/RADIUS REIKSME NERASTA  \n";
+					}
+						else std::cerr<<"Klaida   DISTRIBUTION/RMIN REIKSME NERASTA  \n";
+				}
+					else std::cerr<<"Klaida   DISTRIBUTION/RMAX REIKSME NERASTA  \n";
+		
 }
 
 
